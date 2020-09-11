c1e248a6cc3b8ac52dce932000ff767814930179
Oliver Levy <oliver.levy.ol@gmail.com>
2020-09-11 12:37:48 -0400

    adding some file architecture

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Header.scss";
+
+export default function Header() {
+  return <div>
+    <p>this is the header</p>
+  </div>;
+}
---
+++ client/src/pages/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Main.scss";
+
+export default function Main() {
+  return (
+    <div>
+      <p>this is the main</p>
+    </div>
+  );
+}
