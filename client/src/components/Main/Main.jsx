4575bac281464a909e2ef07d725f423e063433e2
Oliver Levy <oliver.levy.ol@gmail.com>
2020-09-11 12:40:57 -0400

    made some changes to the file architecture

--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Main.scss";
 
+import Income from "../Income/Income";
+
 export default function Main() {
   return (
     <div>
       <p>this is the main</p>
+      <Income />
     </div>
   );
 }
