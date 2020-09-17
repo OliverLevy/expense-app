2f432babff705899cc3579e2af71c1dc33de09aa
Oliver Levy <oliver.levy.ol@gmail.com>
2020-09-17 17:50:35 -0400

    added about page info and social link

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,27 +1,46 @@
 import React from "react";
 import "./About.scss";
 
+import email from "./email.png";
+import instagram from "./instagram.png";
+import linkedin from "./linkedin.png";
+
 export default function About() {
   return (
     <section className="about">
       <article>
         <h2>About the Expense Tracker</h2>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error nulla
-          alias voluptatem neque vel voluptate adipisci, aliquam sequi
-          consequatur fugit sit ab blanditiis id! Optio velit a illum
-          laboriosam. Culpa.
+          Hi, my name's Oli! If you like this app, and want to work with me /
+          hire me, I'd love to hear from you.
         </p>
-      </article>
-      <article>
-        <h2>About Oliver Levy</h2>
+        <br />
+
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error nulla
-          alias voluptatem neque vel voluptate adipisci, aliquam sequi
-          consequatur fugit sit ab blanditiis id! Optio velit a illum
-          laboriosam. Culpa.
+          I made <i>Expense Tracker</i> to experiment with React Hooks and
+          Firebase.
+          <br />
+          To use the app, you must sign in with either Google or Facebook.
+          <br />
+          Once in the app, you can add your monthly income and expenses to track
+          your monthly spending. That's it! <br />
+          <br />
+          *All your information is secure in firebase and will never be used for
+          anything outside of this application.
         </p>
       </article>
+
+      <div className="social-icon__container">
+        <a href="mailto:oliver.levy.ol@gmail.com">
+          <img src={email} alt="email icon" className="social-icon" />
+        </a>
+        <a href="https://www.instagram.com/oli_levy/">
+          <img src={instagram} alt="instagram icon" className="social-icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/oliver-levy/">
+          <img src={linkedin} alt="linkedin icon" className="social-icon" />
+        </a>
+      </div>
     </section>
   );
 }
